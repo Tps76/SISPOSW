7f8c478c71714f7af92ba35257d406657b338ed6
Dany <dany.mrtinez@gmail.com>
2018-09-10 15:48:23 -0500

    update plantilla

---
+++ Vistas/template.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.9/css/mdb.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="views/css/style.css">
+    <title>
+        <?php  
+            if (isset($_SESSION['admin'])) {
+                echo "Admin - Dashboard";
+            }elseif(isset($_SESSION['empleado'])){
+                echo "Dashboard";
+            }else{
+                echo "Tienda Virtual - SISPOSW";
+            }
+        ?>
+    </title>
+</head>
+<body>
+    <?php 
+        if (isset($_SESSION['admin']) || isset($_SESSION['empleado'])) {
+        
+    ?>
+    <!-- Dashboard Admin y empleado -->
+    <header class="row bg-info header shadow">
+        <?php include "views/modules/header.php"; ?>
+    </header>
+    <section class="mx-0 row">
+        <?php include "views/modules/nav.php"; ?>
+        <main class="col-md-10">
+            <?php 
+            adminController::RutaController();
+            ?>
+        </main>
+    </section>
+    <?php 
+        }else{
+    ?>
+    <!-- Tienda Virtual -->
+    <!-- ======= BANNER ======= -->
+    <aside class="container-fluid banner row d-flex mb-3">
+        <!-- <img class="img-fluid" src="src/assets/banner11.jpg" alt=""> -->
+        <h1 class="h1 text-center col-md-12">
+        <?php
+        session_start();
+        if (isset($_GET["msg"])) {
+            if ($_GET["msg"] == "") {
+                echo "BANNER";
+            } else {
+                echo $_GET["msg"];
+            }
+        } else {
+            echo "BANNER";
+        }
+        ?>
+        </h1>
+    </aside>
+    <!-- ======= HEADER SEARCH BAR ======= -->
+    <header class="container-fluid row n">
+        <?php include "modules/header.php" ?>
+    </header>
+    
+    <div class="container-fluid row">
+        <!-- ======= MAIN MENU ======= -->
+        <nav class="col-md-2 d">
+            <?php include "modules/categories.php"; ?>
+        </nav>
+        <!-- ======= MAIN CONTNTENT ======= -->
+        <main class="col-md-10">
+            <?php include "modules/products.php" ?>
+        </main>
+    </div>
+    <?php
+        }
+    ?>
+    <!-- Librerias (Jquery, Jquery-UI, Popper, Bootstrap 4) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    
+    <!-- funciones propias -->
+    <script src="src/js/function.js"></script>
+    <script src="src/js/selects.js"></script>
+    <script src="src/js/validar.js"></script>
+</body>
+</html>
