e779ad1a94fb94e9a9df4ea34783a3d94655c8f3
Camilo <camilov1927@gmail.com>
2018-09-10 15:35:35 -0500

    Models

--- Modelo/Process/categoria.php
+++ Modelo/Process/categoria.php
@@ -3,9 +3,58 @@ require_once '../Include/conexion.php'
 
 class categoria{
     public static getCategoriaProductoActivo(){
-        $sql = "SELECT idcatproducto,nombre_categoria FROM categoria_producto WHERE estado_categoria = '".true."'";
-        $resultado = connection::getInstance()->getBD()->prepare($consultar);
-        $resultado->execute();
-        $tabla = $resultado;
+        $sql = "SELECT idcatproducto,nombre_categoria FROM categoria_producto WHERE estado_categoria = true";
+        try{
+            $resultado = connection::getInstance()->getBD()->prepare($sql);
+            $resultado->execute();
+            $tabla = $resultado->fetchAll(PDO::FETCH_ASSOC);
+            return $tabla;
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+
+    public static insertCategoriaProducto($nombre){
+        $sql = "INSERT INTO categoria_producto(nombre_categoria) VALUES(?)";
+        try{
+            $resultado = connection::getInstance()->getBD()->prepare($sql);
+            $resultado->execute(array($nombre));
+            return true;
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+
+    public static updateCategoriaProducto($nombre){
+        $sql = "UPDATE categoria_producto SET nombre_categoria = ?";
+        try{
+            $resultado = connection::getInstance()->getBD()->prepare($sql);
+            $resultado->execute(array($nombre));
+            return true;
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+
+    public static desactivarCategoriaProducto($id){
+        $sql = "UPDATE categoria_producto SET estado_categoria = false WHERE idcatproducto = ?";
+        try{
+            $resultado = connection::getInstance()->getBD()->prepare($sql);
+            $resultado->execute(array($id));
+            return true;
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+
+    public static activarCategoriaProducto($id){
+        $sql = "UPDATE categoria_producto SET estado_categoria = true WHERE idcatproducto = ?";
+        try{
+            $resultado = connection::getInstance()->getBD()->prepare($sql);
+            $resultado->execute(array($id));
+            return true;
+        }catch(PDOException $e){
+            return false;
+        }
     }
 }
