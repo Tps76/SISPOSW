e5034bad7ae9744a80b6965508b31b5a63c1c59b
Dany <dany.mrtinez@gmail.com>
2018-09-10 15:48:36 -0500

    update

--- Controlador/core.php
+++ Controlador/core.php
@@ -1,1 +1,8 @@
 <?php
+
+// Requires necesarios 
+// Controladores
+require_once "adminController.php";
+// Modelos
+require_once "Modelo/Include/conexion.php";
+require_once "Modelo/Include/config.php";
