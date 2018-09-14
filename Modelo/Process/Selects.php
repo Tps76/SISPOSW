6d3992fa10fb2989bf6f14562b1377a541df5e8b
Dany <dany.mrtinez@gmail.com>
2018-09-14 14:07:18 -0500

    update Ajax

--- Modelo/Process/Selects.php
+++ Modelo/Process/Selects.php
@@ -2,5 +2,45 @@
 
 class Selects
 {
-    // Select
+    // País
+    public function getPais()
+    {
+        $consulta = "SELECT * FROM pais WHERE estado_pais = 1 ORDER BY nombre_pais ASC";
+        $con = connection::getInstance();
+        $db = $con->getBD();
+        $stmt = $db->prepare($consulta);
+        if ($stmt->execute()) {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            return "error";
+        }
+    }
+    // Departamento
+    public function getDepto($id)
+    {
+        $consulta = "SELECT * FROM departamento WHERE estado_departamento = 1 AND idpais = ? ORDER BY nombre_departamento ASC";
+        $con = connection::getInstance();
+        $db = $con->getBD();
+        $stmt = $db->prepare($consulta);
+        $stmt->bindParam(1,$id,PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }else{
+            return "error";
+        }
+    }
+    // Ciudad
+    public function getCiudad($id)
+    {
+        $consulta = "SELECT * FROM ciudad WHERE estado_ciudad = 1 AND iddepartamento = ? ORDER BY nombre_ciudad ASC";
+        $con = connection::getInstance();
+        $db = $con->getBD();
+        $stmt = $db->prepare($consulta);
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            return "error";
+        }
+    }
 }
---
+++ Vistas/js/Ajax.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $("#pais").on("change", function () {
+        let idPais = $(this).val();
+        let url = "Modelo/Functions/Ajax.php";
+        if (idPais) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: "idPais="+idPais,
+                success: function (response) {
+                    // console.log(response);
+                    $("#depto").html(response);
+                    $("#ciudad").html('<option value="">Seleccione Departamento primero</option>');
+                }
+            });
+        }else{
+            $("#depto").html('<option value="">Seleccione País primero</option>');
+            $("#ciudad").html('<option value="">Seleccione Departamento primero</option>');
+        }
+    });
+    $("#depto").on("change", function () {
+        let idDepto = $(this).val();
+        let url = "Modelo/Functions/Ajax.php";
+        if (idDepto) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: "idDepto="+idDepto,
+                success: function (response) {
+                    $("#ciudad").html(response);
+                }
+            });
+        }else{
+            $("#ciudad").html('<option value="">Seleccione Departamento primero</option>');
+        }
+    });
+});
