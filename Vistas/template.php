fc6a50a417d614e930766401ed5d357f4b16d399
Dany <dany.mrtinez@gmail.com>
2018-09-14 14:07:41 -0500

    add ajax.js

--- Vistas/template.php
+++ Vistas/template.php
@@ -36,6 +36,17 @@
             ?>
         </main>
     </section>
+    <!-- Librerias (Jquery, Jquery-UI, Popper, Bootstrap 4) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script> -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    
+    <!-- funciones propias -->
+    <script src="Vistas/js/Ajax.js"></script>
+    <!-- <script src="Vistas/js/selects.js"></script>
+    <script src="Vistas/js/function.js"></script>
+    <script src="Vistas/js/validar.js"></script> -->
     <?php 
         // }else{
     ?>
