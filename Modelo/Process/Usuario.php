624d4f0ae79883e1c5840fb05c76056a205fab54
Dany <dany.mrtinez@gmail.com>
2018-09-14 22:47:43 -0500

    fix

--- Modelo/Process/Usuario.php
+++ Modelo/Process/Usuario.php
@@ -3,14 +3,14 @@
 class Usuario
 {
     /* =========== REGISTRO ============ */
-    public function regId($tipo, $numero)
+    public function regId($datos)
     {
         $consulta = "INSERT INTO identidad (tipo_identidad, numero_identidad) VALUES (?, ?)";
         $con = Connection::getInstance();
         $db = $con->getBD();
         $stmt = $db->prepare($consulta);
-        $stmt->bindParam(1,$tipo,PDO::PARAM_STR);
-        $stmt->bindParam(2,$numero,PDO::PARAM_INT);
+        $stmt->bindParam(1, $datos['tipo_id'], PDO::PARAM_STR);
+        $stmt->bindParam(2, $datos['id'], PDO::PARAM_INT);
         if ($stmt->execute()) {
             return "success";
         }else{
@@ -18,7 +18,7 @@ class Usuario
         }
         $stmt->close();
     }
-    public function regPersona($datos)
+    public function regPersona($datos, $id)
     {
         // Consulta a realizar.
         $consulta = "INSERT INTO persona (ididentidad, nombre_persona, apellido_persona, genero, fecha_nacimiento, idciudad, direccion, telefono) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
@@ -29,7 +29,7 @@ class Usuario
         // PDOStatement.
         $stmt = $db->prepare($consulta);
         // Parseo y asignación de datos.
-        $stmt->bindParam(1, $datos['id'], PDO::PARAM_INT);
+        $stmt->bindParam(1, $id['ididentidad'], PDO::PARAM_INT);
         $stmt->bindParam(2, $datos['nombre'], PDO::PARAM_STR);
         $stmt->bindParam(3, $datos['apellido'], PDO::PARAM_STR);
         $stmt->bindParam(4, $datos['genero'], PDO::PARAM_STR);
@@ -48,8 +48,44 @@ class Usuario
     }
     public function regUsuario($datos)
     {
-        $consulta = "";
+        $consulta = "INSERT INTO usuario(email_usuario, password_usuario, tipo_usuario) VALUES (?,?,?)";
+        // Establece la conexion.
+        $con = Connection::getInstance();
+        // Ingresa base de datos.
+        $db = $con->getBD();
+        // PDOStatement.
+        $stmt = $db->prepare($consulta);
+        // Parseo y asignación de datos.
+        $stmt->bindParam(1, $datos['email'], PDO::PARAM_STR);
+        $stmt->bindParam(2, $datos['pass'], PDO::PARAM_STR);
+        $stmt->bindParam(3, $datos['tipo'], PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            return "success";
+        }else{
+            return "error";
+        }
+        $stmt->close();
     }
+    public function regCliente($idUsuario, $idPersona)
+    {
+        $consulta = "INSERT INTO cliente(idpersona, idusuario) VALUES (?,?)";
+        // Establece la conexion.
+        $con = Connection::getInstance();
+        // Ingresa base de datos.
+        $db = $con->getBD();
+        // PDOStatement.
+        $stmt = $db->prepare($consulta);
+        // Parseo y asignación de datos.
+        $stmt->bindParam(1, $idPersona['idpersona'], PDO::PARAM_INT);
+        $stmt->bindParam(2, $idUsuario['idusuario'], PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            return "success";
+        } else {
+            return "error";
+        }
+        $stmt->close();
+    }
+    
     /* =========== SELECCIONAR ============ */
     public function getId($numero)
     {
@@ -59,7 +95,7 @@ class Usuario
         $stmt = $db->prepare($consulta);
         $stmt->bindParam(1,$numero,PDO::PARAM_INT);
         if ($stmt->execute()) {
-            return $stmt->fetchColumn();
+            return $stmt->fetch(PDO::FETCH_ASSOC);
         }else {
             return "error";
         }
@@ -71,9 +107,9 @@ class Usuario
         $con = Connection::getInstance();
         $db = $con->getBD();
         $stmt = $db->prepare($consulta);
-        $stmt->bindParam(1,$id,PDO::PARAM_INT);
+        $stmt->bindParam(1,$id['ididentidad'],PDO::PARAM_INT);
         if ($stmt->execute()) {
-            return $stmt->fetchColumn();
+            return $stmt->fetch(PDO::FETCH_ASSOC);
         }else {
             return "error";
         }
@@ -81,18 +117,28 @@ class Usuario
     }
     public function getIdUsuario($email)
     {
-        // code...
+        $consulta = "SELECT idusuario FROM usuario WHERE email_usuario = ?";
+        $con = Connection::getInstance();
+        $db = $con->getBD();
+        $stmt = $db->prepare($consulta);
+        $stmt->bindParam(1, $email, PDO::PARAM_STR);
+        if ($stmt->execute()) { 
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            return "error";
+        }
+        $stmt->close();
     }
     /* =========== LOGIN USER ============ */
     public function loginUser($datos)
     {
-        $consulta = "SELECT nickname, pass FROM users WHERE pass = ?";
+        $consulta = "SELECT email_usuario, password_usuario, tipo_usuario FROM usuario WHERE password_usuario = ?";
         $con = Connection::getInstance();
         $db = $con->getBD();
         $stmt = $db->prepare($consulta);
         $stmt->bindParam(1,$datos['pass'],PDO::PARAM_STR);
         if ($stmt->execute()) {
-            return $stmt->fetch();
+            return $stmt->fetch(PDO::FETCH_ASSOC);
         }else {
             return "error";
         }
