2643eb1889f1a91814fd4968428fa4ca50c8ebbc
Dany <dany.mrtinez@gmail.com>
2018-09-15 20:47:48 -0500

    coment bootstrap material design

--- Vistas/template.php
+++ Vistas/template.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.9/css/mdb.min.css" rel="stylesheet">
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.9/css/mdb.min.css" rel="stylesheet"> -->
     <link rel="stylesheet" href="Vistas/css/style.css">
     <title>
         <?php  
@@ -21,7 +21,7 @@
 </head>
 <body>
     <?php 
-        if (isset($_SESSION['admin']) || isset($_SESSION['empleado'])) {
+        // if (isset($_SESSION['admin']) || isset($_SESSION['empleado'])) {
         
     ?>
     <!-- ========================== 
@@ -53,59 +53,59 @@
     <script src="Vistas/js/function.js"></script>
     <script src="Vistas/js/validar.js"></script>
     <?php 
-         }else{
+        //  }else{
     ?>
     <!-- ======================
             Tienda Virtual 
          ====================== -->
     <!-- ======= BANNER ======= -->
 
-    <aside class="container-fluid banner row d-flex mb-3">
+    <!-- <aside class="container-fluid banner row d-flex mb-3">
         <img class="img-fluid" src="src/assets/banner11.jpg" alt="">
-        <h1 class="h1 text-center col-md-12">
+        <h1 class="h1 text-center col-md-12"> -->
         <?php
-        session_start();
-        if (isset($_GET["msg"])) {
-            if ($_GET["msg"] == "") {
-                 echo "BANNER";
-             } else {
-                 echo $_GET["msg"];
-             }
-         } else {
-             echo "BANNER";
-        }
+        // session_start();
+        // if (isset($_GET["msg"])) {
+        //     if ($_GET["msg"] == "") {
+        //          echo "BANNER";
+        //      } else {
+        //          echo $_GET["msg"];
+        //      }
+        //  } else {
+        //      echo "BANNER";
+        // }
         ?>
-        </h1>
-    </aside>
+        <!-- </h1>
+    </aside> -->
     <!-- ======= HEADER SEARCH BAR ======= -->
-    <header class="container-fluid row n">
-        <?php include "modules/header.php" ?>
-    </header>
-    <!-- ======= FIN HEADER SEARCH BAR ======= -->
+    <!-- <header class="container-fluid row n">  -->
+        <?php //include "modules/header.php" ?>
+    <!-- </header> -->
+    <!--======= FIN HEADER SEARCH BAR ======= -->
     
-    <div class="container-fluid row">
+    <!-- <div class="container-fluid row"> -->
 
 
         <!-- ======= MAIN MENU ======= -->
-        <nav class="col-md-2 d">
-            <?php include "modules/categories.php"; ?>
-        </nav>
+        <!-- <nav class="col-md-2 d">  -->
+            <?php //include "modules/categories.php"; ?>
+        <!-- </nav> -->
         <!-- ======= MAIN CONTNTENT ======= -->
-        <main class="col-md-10">
-            <?php include "modules/products.php" ?>
-        </main>
-    </div>
+        <!-- <main class="col-md-10"> -->
+            <?php //include "modules/products.php" ?>
+        <!-- </main>
+    </div> -->
     <?php
-         }
+        //  }
     ?>
     <!-- Librerias (Jquery, Jquery-UI, Popper, Bootstrap 4) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
     <!-- funciones propias -->
-    <script src="Vistas/js/selects.js"></script>
+    <!-- <script src="Vistas/js/selects.js"></script>
     <script src="Vistas/js/function.js"></script>
-    <script src="Vistas/js/validar.js"></script>
+    <script src="Vistas/js/validar.js"></script> -->
 </body>
 </html>
