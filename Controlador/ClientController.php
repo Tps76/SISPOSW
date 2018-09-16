5ecd3548d1e62cb3464708b6b1baeee6013252a4
Dany <dany.mrtinez@gmail.com>
2018-09-16 18:51:52 -0500

    add controller

---
+++ Controlador/ClientController.php
@@ -0,0 +1,85 @@
+<?php
+
+class ClientController
+{
+    public static function logIn()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            // $id = $login[''];
+            if (isset($_POST['']) && isset($_POST [''])) {
+
+                if ($login != "error") {
+                    $login = Usuario::loginUser($datos);
+                    if($login['tipo_usuario'] == 1){
+                        $_SESSION['admin'] = true;
+                    }elseif($login['tipo_usuario'] == 2){
+                        $_SESSION['empleado'] = true;
+                    }else{
+                        $_SESSION['cliente'] = true;
+                        // $_SESSION['cliente'][];
+                    }
+                }
+            }
+
+        }
+    }
+    public static function register()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            if (isset($_POST['tipo_identificacion'])) {
+
+                $datos = array(
+                    "id" => $_POST['id'],
+                    "tipo_id" => $_POST['tipo_identificacion'],
+                    "nombre" => $_POST['name'],
+                    "apellido" => $_POST['last-name'],
+                    "genero" => $_POST['genero'],
+                    "fecha" => $_POST['date'],
+                    "ciudad" => $_POST['ciudad'],
+                    "dir" => $_POST['dir'],
+                    "tel" => $_POST['cel'],
+                    "email" => $_POST['email'],
+                    "pass" => $_POST['pass'],
+                    "tipo" => 3
+                );
+                $identidad = Usuario::regId($datos);
+                $usuario = Usuario::regUsuario($datos);
+                if ($identidad && $usuario) {
+
+                    $id = Usuario::getId($_POST['id']);
+                    $email = $_POST['email'];
+                    $idUsuario = Usuario::getIdUsuario($email);
+                    if ($id && $idUsuario) {
+                        $persona = Usuario::regPersona($datos, $id);
+
+                        if ($persona) {
+                            $idPersona = Usuario::getIdPersona($id);
+
+                            if ($idPersona) {
+                                $cliente = Usuario::regCliente($idUsuario, $idPersona);
+                                if ($cliente) {
+                                    header("location:index.php?action=index");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    public static function getProd()
+    {
+        # code...
+    }
+
+    public static function getCat()
+    {
+        # code...
+    }
+
+    public static function searchProds()
+    {
+        # code...
+    }
+}
