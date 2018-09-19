749c6854bc29139125cb8fd8ccf39f212ffee5e9
Dany <dany.mrtinez@gmail.com>
2018-09-18 22:54:06 -0500

    campos cliente ok

--- Modelo/Functions/Ajax.php
+++ Modelo/Functions/Ajax.php
@@ -1,28 +1,40 @@
 <?php
 require_once "../../Controlador/adminController.php";
 require_once "../Process/Selects.php";
+require_once "../Process/Usuario.php";
 require_once "../Include/conexion.php";
 
 class Ajax
 {
     public function selectCiudad($id)
     {
-        $answer = adminController::selectCiudad($id);
-        echo $answer;
+        $ciudad = adminController::selectCiudad($id);
+        echo $ciudad;
     }
     public function selectDepto($id)
     {
         $depto = adminController::selectDepto($id);
         echo $depto;
     }
+    public function fieldsCliente($id)
+    {
+        $fields = Usuario::getCliente($id, "");
+        $hola = json_encode($fields);
+        echo $hola;
+    }
 }
-if ($_POST['idPais']) {
+if (isset($_POST['idPais'])) {
     $id = $_POST['idPais'];
     $pais = new Ajax();
     $pais->selectDepto($id);
 }
-if ($_POST['idDepto']) {
+if (isset($_POST['idDepto'])) {
     $id = $_POST['idDepto'];
     $depto = new Ajax();
     $depto->selectCiudad($id);
+}
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+    $modify = new Ajax();
+    $modify->fieldsCliente($id);
 }
--- Vistas/js/Ajax.js
+++ Vistas/js/Ajax.js
@@ -34,6 +34,50 @@ $(document).ready(function () {
             $("#ciudad").html('<option value="">Seleccione primero Departamento</option>');
         }
     });
-    /*========== Remover producto carrito ===========*/
-    
+    /* =================================
+        MODIFICACIÓN CLIENTE FULL FIELDS 
+       ================================= */
+    $(".cliente").click(function () {
+        let val = $(this).val();
+        let url = "Modelo/Functions/Ajax.php";
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: "id="+val,
+            // contentType = false,
+            dataType: "json",
+            success: function (response) {
+                if (response) {
+                    
+                    $("[name=id]").attr("value", response['numero_identidad']);
+                    $("[name=contacto]").attr("value", response['telefono']);
+                    $("[name=nombre]").attr("value", response['nombre_persona']);
+                    $("[name=apellido]").attr("value", response['apellido_persona']);
+                    $("[name=email]").attr("value", response['email_usuario']);
+                    $("[name=date]").attr("value", response['fecha_nacimiento']);
+                    $("[name=dir]").attr("value", response['direccion']);
+                    if (response['genero'] == "M") {
+                        $("[value=M]").prop('checked',true);
+                    } else {
+                        $("[value=F]").prop('checked',true);
+                    }                                
+                }
+            }
+        })
+        .fail(function () {
+            console.log("error");
+        });
+    });
+    /* ===================================
+        MODIFICACIÓN EMPLEADOS FULL FIELDS 
+       =================================== */
+    /* ===================================
+        MODIFICACIÓN PRODUCTOS FULL FIELDS 
+       =================================== */
+    /* ===================================
+        MODIFICACIÓN CATEGORIA FULL FIELDS 
+       =================================== */
+    /* =====================================
+        MODIFICACIÓN PROVEEDORES FULL FIELDS 
+       ===================================== */
 });
